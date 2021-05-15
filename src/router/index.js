27e5d2ba69e3ae3a955511decbae7f469067b987
Tobias von Dewitz <tobias@scraperwall.com>
2021-05-15 15:31:57 +0200

    cleanup

--- src/router/index.js
+++ src/router/index.js
@@ -72,36 +72,6 @@ function configRoutes () {
         },
       ]
     },
-    {
-      path: '/pages',
-      redirect: '/pages/404',
-      name: 'Pages',
-      component: {
-        render (c) { return c('router-view') }
-      },
-      children: [
-        {
-          path: '404',
-          name: 'Page404',
-          component: Page404
-        },
-        {
-          path: '500',
-          name: 'Page500',
-          component: Page500
-        },
-        {
-          path: 'login',
-          name: 'Login',
-          component: Login
-        },
-        {
-          path: 'register',
-          name: 'Register',
-          component: Register
-        }
-      ]
-    },
   ]
 }
 
